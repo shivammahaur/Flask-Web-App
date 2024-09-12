1aa1dcaad50ba89ede813d88b6a17b8c7dc269ed
Shivam Mahaur <mahaurdesignstudio@gmail.com>
2024-09-12 18:48:01 +0530

    handling POST requests chapter complete

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -10,6 +10,24 @@ def login():
 def logout():
     return "<p>LOGOUT</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            # add user to db
+            pass
+        
     return render_template("sign_up.html")

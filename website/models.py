67ad70327021c3f29a7b0c7d47d4809987d381c4
Shivam Mahaur <mahaurdesignstudio@gmail.com>
2024-09-12 19:33:51 +0530

    Database models chapter complete

--- website/models.py
+++ website/models.py
@@ -1,6 +1,15 @@
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    noteID = db.Column(db.Integer, primary_key=True)
+    noteData = db.Column(db.String(10000))
+    noteDate = db.Column(db.DateTime(timezone=True), default=func.now())
+    
 
 class User(db.Model, UserMixin):
     userID = db.Column(db.Integer, primary_key=True)
     userEmail = db.Column(db.String(150), unique=True)
+    userPassword = db.Column(db.String(150))
+    userFirstName = db.Column(db.String(150))

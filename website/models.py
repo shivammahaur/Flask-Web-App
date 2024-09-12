4b3cccb83a440f4e64cab749a35a031747c6c4a3
Shivam Mahaur <mahaurdesignstudio@gmail.com>
2024-09-12 19:42:21 +0530

    Foreign relationships chapter complete

--- website/models.py
+++ website/models.py
@@ -6,10 +6,12 @@ class Note(db.Model):
     noteID = db.Column(db.Integer, primary_key=True)
     noteData = db.Column(db.String(10000))
     noteDate = db.Column(db.DateTime(timezone=True), default=func.now())
-    
+    userID = db.Column(db.Integer, db.ForeignKey('user.userID'))
+
 
 class User(db.Model, UserMixin):
     userID = db.Column(db.Integer, primary_key=True)
     userEmail = db.Column(db.String(150), unique=True)
     userPassword = db.Column(db.String(150))
     userFirstName = db.Column(db.String(150))
+    notes = db.relationship('Note')

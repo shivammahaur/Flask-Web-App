f516b914b16f3bcc148341e7e55a6a2720a822b3
Shivam Mahaur <mahaurdesignstudio@gmail.com>
2024-09-12 20:08:29 +0530

    Database creation chapter complete

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+
 
 db = SQLAlchemy()
 DB_NAME = "databse.db"
@@ -10,7 +12,7 @@ def create_app():
     '''
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{path.join(app.root_path,DB_NAME)}'
     db.init_app(app)
 
     from .views import views
@@ -19,4 +21,15 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    create_database(app)
+
     return app
+
+
+def create_database(app):
+    if not path.exists('website/'+DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print('Created Database!')
